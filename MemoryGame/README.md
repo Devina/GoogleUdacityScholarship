1cc326336fa4925a447975e1bd60f60e52790a57
Devina <36676408+Devina9@users.noreply.github.com>
2018-03-24 18:33:23 +0000

    Memory Game Project

---
+++ MemoryGame/README.md
@@ -0,0 +1,21 @@
+# Memory Game Project
+
+## Instructions
+
+This is a memory game. Clicking the tiles will reveal icons. If two icons which are open match, then these cards will remain open. If they do not match, the tiles will be reversed again.
+
+There is a stars rating present which will be provided upon completing the game. This is dependant on the number of moves to complete the game. The rating is as such:
+* **1-10 moves:** 3 stars.
+* **11-20 moves:** 2 stars.
+* **21-30 moves:** 1 star.
+* **30+ moves:** 0 star.
+
+To win this game, all tiles should be matched.
+
+## External libraries
+
+This game utilises the following external libraries within the code.
+
+* [jQuery](https://jquery.com)
+* [Sweet Alert](https://sweetalert.js.org)
+* [Animate.css](https://daneden.github.io/animate.css/)
---
+++ MemoryGame/js/app.js
@@ -0,0 +1,193 @@
+/*-------------------------------------
+    Variables
+--------------------------------------*/
+let deck = $('.deck');
+let moves = $('.moves');
+let ratingStars = $('.fa-star');
+let restart = $('.restart');
+let timer = $('.timer');
+const cardsList = ['anchor', 'anchor', 'bicycle', 'bicycle', 'bolt', 'bolt', 'bomb', 'bomb', 'cube', 'cube', 'diamond', 'diamond', 'leaf', 'leaf', 'paper-plane-o', 'paper-plane-o'];
+const cardCount = cardsList.length / 2;
+let openedCards = [];
+let match = 0;
+let movesCount = 0;
+let currentTimer;
+let second = 0;
+let firstClick = false;
+const threeStars = 10, twoStars = 20, oneStar = 30;
+
+/*-------------------------------------
+    Functions
+--------------------------------------*/
+
+//Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+//Function - Main game play
+function gamePlay() {
+  let cards = shuffle(cardsList);
+  deck.empty();
+  match = 0;
+  movesCount = 0;
+  moves.text('0');
+  ratingStars.removeClass('fa-star-o').addClass('fa-star');
+
+  //Creates the randomised deck
+  for (let i = 0; i < cards.length; i++) {
+    deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
+  }
+
+  cardListener();
+
+  //Reset timer
+  stopTimer(currentTimer);
+  second = 0;
+  timer.text(`${second}`)
+};
+
+//Function - Start timer
+function startTimer() {
+  currentTimer = setInterval(function () {
+    timer.text(`${second}`)
+    second += 1
+  }, 1000);
+}
+
+//Function - Stop timer
+function stopTimer(timer) {
+  if (timer) {
+    clearInterval(timer);
+  }
+}
+
+//Function - Star rating
+function starRating(movesCount) {
+  let rating = 3;
+  if (movesCount > threeStars && movesCount < twoStars) {
+    reduceStar(2);
+    rating = 2;
+  } else if (movesCount > twoStars && movesCount < oneStar) {
+    reduceStar(1);
+    rating = 1;
+  } else if (movesCount > oneStar) {
+    reduceStar(0);
+    rating = 0;
+  }
+  return { score: rating };
+};
+
+//Function - Reduce star based on count
+function reduceStar(starNumber) {
+  ratingStars.eq(starNumber).removeClass('fa-star').addClass('fa-star-o');
+}
+
+//Function - End game modal
+function endGame(movesCount, score) {
+  stopTimer(currentTimer);
+
+  swal({
+    closeOnEsc: false,
+    closeOnClickOutside: false,
+    title: 'Congratulations! You Won!',
+    text: 'With ' + movesCount + ' Moves and ' + score + ' Stars in ' + (second-1) + ' Seconds.\n Woooooo!',
+    icon: 'success',
+    buttons: {
+      playAgain: {
+        text: 'Play again!'
+      }
+    }
+  }).then(function (isConfirm) {
+    if (isConfirm) {
+      firstClick = false;
+      gamePlay();
+    }
+  })
+}
+
+//Function - Card listener
+function cardListener() {
+
+  //Click Event - Flip card
+  deck.find('.card').bind('click', function () {
+
+    //Start timer on first click
+    if(firstClick === false) {
+      firstClick = true;
+      startTimer();
+    }
+
+    //Current flipped card
+    let currentCard = $(this);
+    let card = currentCard.context.innerHTML;
+    currentCard.addClass('open show');
+    openedCards.push(card);
+
+    //Compare with opened card
+    if (openedCards.length === 2) {
+      if (card === openedCards[0]) {
+        deck.find('.open').addClass('match animated rubberBand');
+        setTimeout(function () {
+          deck.find('.match').removeClass('open show');
+        }, 400);
+        match++;
+      } else {
+        deck.find('.open').addClass('notmatch animated wobble');
+        setTimeout(function () {
+          deck.find('.open').removeClass('open show notmatch animated wobble');
+        }, 400);
+      }
+      openedCards = [];
+      movesCount++;
+      starRating(movesCount);
+      moves.html(movesCount);
+    }
+
+    //End Game if all cards match
+    if (cardCount === match) {
+      starRating(movesCount);
+      let score = starRating(movesCount).score;
+      setTimeout(function () {
+        endGame(movesCount, score);
+      }, 500);
+    }
+  });
+};
+
+/*-------------------------------------
+    Events
+--------------------------------------*/
+
+//Click Event - Restart game
+restart.bind('click', function () {
+  swal({
+    closeOnEsc: false,
+    closeOnClickOutside: false,
+    title: 'Are you sure you  want to restart?',
+    text: "Your progress will be lost!",
+    icon: 'warning',
+    dangerMode: true,
+    buttons: true
+  }).then(function (isConfirm) {
+    if (isConfirm) {
+      firstClick = false;
+      gamePlay();
+    }
+  })
+});
+
+/*-------------------------------------
+    JS Call
+--------------------------------------*/
+gamePlay();
